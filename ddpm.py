6c08deed72e4e5a93986e7822cb3457786870cb9
mr-gentle-666 <965845592@qq.com>
2025-04-18 20:27:16 +0800

    重构了ddpm，优雅！！！

--- ddpm.py
+++ ddpm.py
@@ -2,8 +2,67 @@ import torch
 import Unet
 import torchvision
 import tqdm
+
+
 def linear_beta_schedule(timesteps=1000):
-    return torch.linspace(0.0001, 0.02, timesteps)
+        scale = 1000 / timesteps
+        return torch.linspace(0.0001*scale, 0.02*scale, timesteps)
+class DDPM:
+    def __init__(self,
+                timesteps=1000,
+                device='cuda',
+    ):
+        self.timesteps = timesteps
+        self.device = device
+        self.beta = linear_beta_schedule(timesteps)  # beta的线性调度
+        self.alpha = 1 - self.beta  # alpha的计算
+        self.alpha_bar = torch.cumprod(self.alpha, dim=0).to(device)
+        pass
+
+    
+    
+    # 前向加噪过程采样
+    def q_sample(self, x_0, t):
+        noise = torch.randn_like(x_0)  # 生成与x_0相同形状的随机噪声
+        alpha_bar_t = self.alpha_bar.gather(dim=0,index=t) # 得到一系列的时间步对应的alpha_bar
+        return noise,alpha_bar_t.sqrt().view(-1, 1, 1, 1) * x_0 + (1 - alpha_bar_t).sqrt().view(-1, 1, 1, 1) * noise #channels需要手动匹配
+
+
+
+    # 反向去噪过程采样
+    def q_posterior(self, x_t, x_0, t):
+        pass
+    
+    def train(
+            self,
+            model,
+            data,
+            loss,
+            optimizer,
+            epochs=10,
+            device='cuda',
+    ):
+        T = self.timesteps
+        for epoch in range(epochs):
+            processBar = tqdm.tqdm(data,unit='step')
+            for step,(x_0, _) in enumerate(processBar):
+               t = torch.randint(1, T, (x_0.shape[0],)).to(device)
+               x_0 = x_0.to(device)
+               noise,x_t = self.q_sample(x_0, t) # 采样得到噪声混合图像
+
+               pre_noise = model(x_t,t)
+
+               l = loss(pre_noise, noise) # 计算损失
+               model.zero_grad()
+               l.backward() # 反向传播
+               optimizer.step() 
+
+               processBar.set_description("[%d/%d] Loss: %.4f" % 
+                                       (epoch,epochs,l.item()))       
+            processBar.close()
+        torch.save(model.state_dict(), './model/ddpm_weights.pth')
+
+
 
 def train(
         model,
@@ -64,6 +123,7 @@ if __name__ == '__main__':
     trainDataLoader = torch.utils.data.DataLoader(train_data,batch_size=batch_size,shuffle=True)
     
     train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
-
+    # ddpm = DDPM(timesteps=1000,device=device)
+    # ddpm.train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
     # 保存模型权重
     torch.save(net.state_dict(), './model/ddpm_weights.pth')

d3984f6261eeef351d65f5b08421eae1236a4575
mr-gentle-666 <965845592@qq.com>
2025-04-18 20:44:17 +0800

    新版训练

--- ddpm.py
+++ ddpm.py
@@ -64,43 +64,43 @@ class DDPM:
 
 
 
-def train(
-        model,
-        data,
-        loss_fn,
-        optimizer,
-        epochs=10,
-        device='cuda',
-):
-    T = 1000
-    beta = linear_beta_schedule(T)
-    alpha = 1-beta
-    alpha_bar = torch.cumprod(alpha, dim=0).to(device=device)
-    history = []
-    for epoch in range(epochs):
-        processBar = tqdm.tqdm(data,unit='step')
-        for step, (x_0, _) in enumerate(processBar):
-            # 采样的到噪声混合图像
-            x_0 = x_0.to(device)
-            noise = torch.randn(x_0.shape[0], 1, 28, 28).to(device)
-            t = torch.randint(1, T, (x_0.shape[0],)).to(device)
-            alpha_bar_t = alpha_bar.gather(dim=0,index=t).to(device)
-            x_t = x_0*alpha_bar_t.sqrt().view(-1,1,1,1)+noise*(1-alpha_bar_t).sqrt().view(-1,1,1,1) #channels需要手动匹配
+# def train(
+#         model,
+#         data,
+#         loss_fn,
+#         optimizer,
+#         epochs=10,
+#         device='cuda',
+# ):
+#     T = 1000
+#     beta = linear_beta_schedule(T)
+#     alpha = 1-beta
+#     alpha_bar = torch.cumprod(alpha, dim=0).to(device=device)
+#     history = []
+#     for epoch in range(epochs):
+#         processBar = tqdm.tqdm(data,unit='step')
+#         for step, (x_0, _) in enumerate(processBar):
+#             # 采样的到噪声混合图像
+#             x_0 = x_0.to(device)
+#             noise = torch.randn(x_0.shape[0], 1, 28, 28).to(device)
+#             t = torch.randint(1, T, (x_0.shape[0],)).to(device)
+#             alpha_bar_t = alpha_bar.gather(dim=0,index=t).to(device)
+#             x_t = x_0*alpha_bar_t.sqrt().view(-1,1,1,1)+noise*(1-alpha_bar_t).sqrt().view(-1,1,1,1) #channels需要手动匹配
             
-            # 去噪
-            pre_noise = model(x_t,t)
+#             # 去噪
+#             pre_noise = model(x_t,t)
 
-            # 计算损失并反向传播
-            model.zero_grad()
-            l = loss_fn(pre_noise, noise)
-            l.backward()
-            optimizer.step()
+#             # 计算损失并反向传播
+#             model.zero_grad()
+#             l = loss_fn(pre_noise, noise)
+#             l.backward()
+#             optimizer.step()
             
-            if step == len(data)-1:
-                history.append(l.item())
-            processBar.set_description("[%d/%d] Loss: %.4f" % 
-                                   (epoch,epochs,l.item()))
-        processBar.close()
+#             if step == len(data)-1:
+#                 history.append(l.item())
+#             processBar.set_description("[%d/%d] Loss: %.4f" % 
+#                                    (epoch,epochs,l.item()))
+#         processBar.close()
 if __name__ == '__main__':
     batch_size = 64
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -122,8 +122,8 @@ if __name__ == '__main__':
     train_data = torchvision.datasets.MNIST(root=path,train=True,transform=transform,download=True)
     trainDataLoader = torch.utils.data.DataLoader(train_data,batch_size=batch_size,shuffle=True)
     
-    train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
-    # ddpm = DDPM(timesteps=1000,device=device)
-    # ddpm.train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
+    # train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
+    ddpm = DDPM(timesteps=1000,device=device)
+    ddpm.train(net,trainDataLoader,loss_fn,optimizer,epochs=10,device=device)
     # 保存模型权重
     torch.save(net.state_dict(), './model/ddpm_weights.pth')
